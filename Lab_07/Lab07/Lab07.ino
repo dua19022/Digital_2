ad2e66bfb307fae4613dc004f9d52a702effd267
dua19022 <68084893+dua19022@users.noreply.github.com>
2021-09-27 21:42:19 -0600

    Lab_07

--- Lab_07/Lab07/Lab07.ino
+++ Lab_07/Lab07/Lab07.ino
@@ -1,112 +1,128 @@
-/*
-  SD card test
 
- This example shows how use the utility libraries on which the'
- SD library is based in order to get info about your SD card.
- Very useful for testing a card when you're not sure whether its working or not.
-
- The circuit:
-  * SD card attached to SPI bus as follows:
- ** MOSI - pin 11 on Arduino Uno/Duemilanove/Diecimila
- ** MISO - pin 12 on Arduino Uno/Duemilanove/Diecimila
- ** CLK - pin 13 on Arduino Uno/Duemilanove/Diecimila
- ** CS - depends on your SD card shield or module.
-     Pin 4 used here for consistency with other Arduino examples
-
-
- created  28 Mar 2011
- by Limor Fried
- modified 9 Apr 2012
- by Tom Igoe
- */
-// include the SD library:
+// Libraries
 #include <SPI.h>
 #include <SD.h>
 
-// set up variables using the SD utility library functions:
-Sd2Card card;
-SdVolume volume;
-SdFile root;
+// Define variable
+File root;
+char option = '0';
+uint8_t control = 0;
+char inByte;
 
-const int chipSelect = 32; //cs PIN
+// Prototypes
+void printDirectory(File dir, int numTabs);
+void readText(char dir);
 
 void setup()
 {
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
- 
-SPI.setModule(0);
+  // Iniciar comunicacion serial y esperar a que el puerto respoda:
+  Serial.begin(9600); // baudRate 
+  SPI.setModule(0); //esto indica que puertos se van a usar
 
-  Serial.print("\nInitializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
-  // or the SD library functions will not work.
-  pinMode(10, OUTPUT);     // change this to 53 on a mega
+  Serial.print("Initializing SD card...\n");
 
+  pinMode(PA_3, OUTPUT);  // Se define como output
 
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
+  if (!SD.begin(PA_3)) {
     Serial.println("initialization failed. Things to check:");
     Serial.println("* is a card is inserted?");
     Serial.println("* Is your wiring correct?");
     Serial.println("* did you change the chipSelect pin to match your shield or module?");
     return;
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
   }
+  Serial.println("Initialization suceed...");
+  Serial.println("Wiring is correct and a card is present.");
 
-  // print the type of card
-  Serial.print("\nCard type: ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    return;
-  }
-
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
-  Serial.println(volume.fatType(), DEC);
-  Serial.println();
-
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
-
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
 }
 
+void loop()
+{
+  if (control == 0) {
+    root = SD.open("/");
+    printDirectory(root, 0);
+    Serial.println("\n Write the option you want to see... ");
+    Serial.println(" Option 1 : Rebellion");
+    Serial.println(" Option 2 : Yoshi");
+    Serial.println(" Option 3 : DarthVader");
+    control++; //al aumentar el valor de control se asegura que el mensaje de menu no aparesca hasta que se halla seleccionado
+  }             //una option.
+  if (Serial.available()) {
+    inByte = Serial.read();//se guarda el valor enviado por serial
+  }
+  if (inByte == '1') {//si el valor recibido es un 1
+    option = '1'; //se coloca un 1 a la option 
+    control--; //se coloca en 0 control para que se muestre el menu
+  }
+  if (inByte == '2') {//misma logica que cuando se recibe un 1
+    option = '2';
+    control--;
+  }
+  if (inByte == '3') {//misma logica que cuando se recibe un 1
+    option = '3';
+    control--;
+  }
+  readText(option);
+  option = '0'; //se asegura que option siempre sea 0 para evitar que se muestre valores no deseados
+}
 
-void loop(void) {
+void readText(char dir) {
+  uint8_t seleccion;
+  File archivo;
+  switch (dir) {//Se abre el archivo dependiendo del valor que tenga la variable option 
+    case '1':
+      archivo = SD.open("rebell~1.txt");
+      seleccion = 1; //habilita la rutina de mostrar el contenido
+      break;
+    case '2':
+      archivo = SD.open("yoshi.txt");
+      seleccion = 1;
+      break;
+    case '3':
+      archivo = SD.open("vader.txt");
+      seleccion = 1;
+      break;
+    default:
+      seleccion = 0;//inhabilita la rutina de mostrar el contenido
+      break;
+  }
+  if (seleccion == 1) {//rutina que sirve para mostrar la informacion guardada en el archivo seleccionado
+    if (archivo) {
+      Serial.println(archivo.name());
 
+      // read from the file until there's nothing else in it:
+      while (archivo.available()) {
+        Serial.write(archivo.read());
+      }
+      // close the file:
+      archivo.close();
+    } else {
+      // if the file didn't open, print an error:
+      Serial.println("error opening file...");
+    }
+  }
+}
+
+void printDirectory(File dir, int numTabs) {//en esta rutina se muestra el contenido guardado en la SD
+  dir.rewindDirectory();
+  while (true) {
+
+    File entry =  dir.openNextFile();
+    if (! entry) {
+      // no more files
+      break;
+    }
+    for (uint8_t i = 0; i < numTabs; i++) {
+      Serial.print('\t');
+    }
+    Serial.print(entry.name());
+    if (entry.isDirectory()) {
+      Serial.println("/");
+      printDirectory(entry, numTabs + 1);
+    } else {
+      // files have sizes, directories do not
+      Serial.print("\t\t");
+      Serial.println(entry.size(), DEC);
+    }
+    entry.close();
+  }
 }
